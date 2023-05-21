9721ce57c946fd3b886e5fd0bf22068e0c0f03da
Azad-Dev <akhundovazad@gmail.com>
2023-05-21 21:49:11 +0300

    third heroku

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const waitingListRoutes = require('./routes/waitingListRoutes');
 
 app.use('/waiting-list',waitingListRoutes);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // PORT
