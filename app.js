e9aada7220c7c246479f381e88b567061b1d71ac
AzadAv <87096546+AzadAv@users.noreply.github.com>
2023-05-21 21:23:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const waitingListRoutes = require('./routes/waitingListRoutes');
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'))
+    res.sendFile(path.join(__dirname, 'client/build'))
 })
 // PORT
 const PORT = process.env.PORT || 7777;
